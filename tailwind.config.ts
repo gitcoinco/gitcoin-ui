0453e0e0994c5a6b5a3cbc0532de9bea87b5837f
Hussein Martinez <husse.dev@gmail.com>
2024-10-16 15:47:43 +0700

    chore: added tailwind prefix ui-

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  prefix: "ui-",
   content: ["./lib/**/*.{js,ts,jsx,tsx}"],
   darkMode: ["class", "[data-mode='dark']"],
   theme: {
