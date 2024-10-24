7dc5da0cf0d72a0d7d77f5210b83f0f783397a0c
0xKurt <Kurt@Merbeth.io>
2024-10-24 16:46:59 +0200

    Add CircleStat

--- src/tokens/colors.ts
+++ src/tokens/colors.ts
@@ -8,4 +8,13 @@ export const colors: Colors = {
   },
   white: "#ffffff",
   black: "#000000",
+  yellow: {
+    "300": "#ffea7d",
+  },
+  orange: {
+    "300": "#ff9776",
+  },
+  green: {
+    "300": "#c4f092",
+  },
 };
