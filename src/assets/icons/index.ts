3612943baec221bf2d3205f8334dc48de709d62e
Nick Lionis <nikolaos@gitcoin.co>
2024-10-29 20:18:55 +0200

    new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,20 @@
+// Status Icons
 export { default as CheckIcon } from "./check.svg?react";
-export { default as ClockIcon } from "./clock.svg?react";
 export { default as ExclamationCircleIcon } from "./exclamationCircle.svg?react";
-export { default as SparklesIcon } from "./sparkles.svg?react";
 export { default as XIcon } from "./x.svg?react";
+
+// Time Icons
+export { default as ClockIcon } from "./clock.svg?react";
+export { default as CalendarIcon } from "./calendar.svg?react";
+
+// Miscellaneous Icons
+export { default as SparklesIcon } from "./sparkles.svg?react";
+export { default as VerifiedBadgeIcon } from "./verifiedBadge.svg?react";
+export { default as GlobeIcon } from "./globe.svg?react";
+
+// Social Media Icons
+export { default as GithubIcon } from "./github.svg?react";
+export { default as TwitterIcon } from "./twitter.svg?react";
+
+// Cryptocurrency Icons
+export { default as ETHIcon } from "./eth.svg?react";
