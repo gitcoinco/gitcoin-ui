959a2165d28662dc32b35c00a62cd77a583254fe
Nick Lionis <nikolaos@gitcoin.co>
2024-10-29 20:21:01 +0200

    adds back Project to the new Components folder

---
+++ src/components/project/components/ProjectCard.stories.tsx
@@ -0,0 +1,99 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { expect, within } from "@storybook/test";
+
+import { QueryError, QueryPending, QuerySuccess, singleProject } from "../mocks/objects";
+import { ProjectCard } from "./ProjectCard";
+
+const meta: Meta<typeof ProjectCard> = {
+  title: "Components/Project/ProjectCard",
+  component: ProjectCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof ProjectCard>;
+
+export const Default: Story = {
+  args: {
+    project: singleProject,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const avatar = await canvas.findByAltText("avatar");
+    let src = avatar.getAttribute("src");
+    expect(src).toContain(singleProject.metadata.logoImg);
+
+    const banner = await canvas.findByAltText("banner");
+    src = banner.getAttribute("src");
+
+    expect(src).toContain(singleProject.metadata.bannerImg);
+  },
+};
+
+export const Undefined: Story = {
+  args: {
+    project: undefined,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const card = canvas.getByRole("presentation");
+    expect(card).toBeVisible();
+
+    const avatarElement = canvas.queryAllByAltText("avatar");
+    expect(avatarElement).toHaveLength(0);
+
+    const bannerElement = canvas.queryAllByAltText("banner");
+    expect(bannerElement).toHaveLength(0);
+  },
+};
+
+export const SuccessQuery: Story = {
+  args: {
+    queryResult: QuerySuccess,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const avatar = await canvas.findByAltText("avatar");
+    let src = avatar.getAttribute("src");
+    expect(src).toContain(singleProject.metadata.logoImg);
+
+    const banner = await canvas.findByAltText("banner");
+    src = banner.getAttribute("src");
+
+    expect(src).toContain(singleProject.metadata.bannerImg);
+  },
+};
+
+export const PendingQuery: Story = {
+  args: {
+    queryResult: QueryPending,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const card = canvas.getByRole("presentation");
+    expect(card).toBeVisible();
+
+    const avatarElement = canvas.queryAllByAltText("avatar");
+    expect(avatarElement).toHaveLength(0);
+
+    const bannerElement = canvas.queryAllByAltText("banner");
+    expect(bannerElement).toHaveLength(0);
+  },
+};
+
+export const ErroryQuery: Story = {
+  args: {
+    queryResult: QueryError,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const avatarElement = canvas.queryByAltText("avatar");
+    expect(avatarElement).toBeNull();
+
+    const bannerElement = canvas.queryByAltText("banner");
+    expect(bannerElement).toBeNull();
+  },
+};
---
+++ src/components/project/types/types.ts
@@ -0,0 +1,19 @@
+export interface Project {
+  id: string;
+  name?: string;
+  description?: string;
+  avatarUrl?: string;
+  bannerUrl?: string;
+  anchorAddress?: string;
+  chainId?: number;
+  metadata: ProjectMetadata;
+}
+
+export interface ProjectMetadata {
+  title: string | undefined;
+  logoImg: string | undefined;
+  bannerImg: string | undefined;
+  description: string | undefined;
+  projectGithub: string | undefined;
+  projectTwitter: string | undefined;
+}
