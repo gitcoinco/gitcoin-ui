7feddc4c27dda9c993fb379ea87d56e3a0619b40
Huss Martinez <husse.dev@gmail.com>
2024-11-04 17:35:27 +0700

    feat: project review list (#17)
    * feat: implemented generic ListGrid component
    
    * chore: install moment library
    
    * chore: added grey-image for temporary placeholder
    
    * feat: implemented ProjectReviewList

---
+++ src/features/checker/components/ProjectReviewList/types.ts
@@ -0,0 +1,14 @@
+export interface Review {
+  reviewer: `0x${string}`;
+  approved: boolean;
+}
+
+export interface ProjectReview {
+  id: number;
+  name: string;
+  date: Date;
+  avatarUrl: string;
+  reviews: Review[];
+  aiSuggestion: number;
+  scoreAverage: number;
+}
