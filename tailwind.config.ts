7c00ac43bc5d1fc5a6cc0fa0779610954baa9fea
Hussein Martinez <husse.dev@gmail.com>
2024-11-19 12:54:38 +0700

    chore: extended tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,25 @@ export default withTV({
   darkMode: ["class", "[data-mode='dark']"],
   theme: {
     extend: {
+      height: {
+        "18": "4.5rem",
+      },
+      width: {
+        "102": "25.5rem",
+      },
+      maxWidth: {
+        "105": "26.25rem",
+      },
+      minHeight: {
+        "18": "4.5rem",
+      },
+      zIndex: {
+        "100": "100",
+      },
+      boxShadow: {
+        toast:
+          "0px 24px 94px 0px rgba(0, 0, 0, 0.17), 0px 7.235px 28.338px 0px rgba(0, 0, 0, 0.11), 0px 3.005px 11.77px 0px rgba(0, 0, 0, 0.09), 0px 1.087px 4.257px 0px rgba(0, 0, 0, 0.06)",
+      },
       fontFamily: {
         mono: ["DM Mono", "serif"],
         sans: ["DM Sans", "sans-serif"],
