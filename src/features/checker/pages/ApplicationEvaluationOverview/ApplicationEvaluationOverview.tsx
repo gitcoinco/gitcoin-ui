7e6e0939784b17085a7d1ff949f0ac15ecb347ec
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-11-22 13:07:34 +0530

    update ApplicationOverviewPage

--- src/features/checker/pages/ApplicationEvaluationOverview/ApplicationEvaluationOverview.tsx
+++ src/features/checker/pages/ApplicationEvaluationOverview/ApplicationEvaluationOverview.tsx
@@ -1,20 +1,42 @@
+import { ProjectBanner } from "@/components/project/components/ProjectBanner/ProjectBanner";
+import { Button } from "@/primitives/Button";
+
+import { EvaluationList } from "../../components/EvaluationList/EvaluationList";
+import { useApplicationEvaluations } from "../../hooks/useApplication";
+
 export interface ApplicationEvaluationOverviewProps {
-  projectId: string;
+  chainId: number;
+  roundId: string;
+  applicationId: string;
 }
 
 export const ApplicationEvaluationOverview = ({
-  projectId,
+  chainId,
+  roundId,
+  applicationId,
 }: ApplicationEvaluationOverviewProps) => {
+  const { data, isLoading, error } = useApplicationEvaluations(chainId, roundId, applicationId);
+  if (isLoading) return <div>Loading...</div>;
+  if (error || !data) return <div>Error loading evaluations</div>;
+
+  const project = data?.application.metadata.application.project;
+
   return (
-    <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-[12px]">
-        <div className="font-mono text-2xl font-medium leading-loose text-black">
-          Application Evaluation Overview
-        </div>
-        <div className="font-mono text-base font-normal leading-7 text-grey-900">
-          {`Previous evaluations for ${projectId} here.`}
-        </div>
+    <div className="flex flex-col gap-6">
+      <ProjectBanner
+        bannerImg={project.bannerImg ?? ""}
+        logoImg={project.logoImg ?? ""}
+        avatarPosition="left"
+      />
+      <h1 className="text-3xl font-medium leading-9">{project.title}</h1>
+      <div className="h-0.5 bg-[#EAEAEA]" />
+      <p className="leading-9 text-grey-900">
+        Evaluate this project and see how others have evaluated this project.
+      </p>
+      <div className="px-16">
+        <EvaluationList evaluations={data.applicationEvaluations} />
       </div>
+      <Button variant="primary" value="Perform evaluation" />;
     </div>
   );
 };
