6dd1d27f3c516e29865d54b7c0c19a38629a8a19
Nick Lionis <nikolaos@gitcoin.co>
2024-11-25 09:19:59 +0700

    creates final evaluation page

---
+++ src/features/checker/components/ProjectEvaluationList/ProjectEvaluationList.tsx
@@ -0,0 +1,98 @@
+import { IconLabel } from "@/components/IconLabel";
+import { CircleStat } from "@/primitives/Indicators";
+import { ListGrid, ListGridColumn } from "@/primitives/ListGrid";
+
+import { getReviewsCount } from "../../utils/getReviewsCount";
+import { EvaluationAction, ProjectEvaluationAction } from "../ProjectEvaluationAction";
+import { ProjectStatus } from "../ProjectEvaluationAction";
+import { ProjectReview } from "../ProjectReviewList";
+
+export interface ProjectEvaluationListProps {
+  evaluationStatus?: ProjectStatus;
+  projects: ProjectReview[];
+  action: (projectId: string, action: EvaluationAction) => void;
+}
+
+export const ProjectEvaluationList = ({
+  evaluationStatus = "pending",
+  projects,
+  action,
+}: ProjectEvaluationListProps) => {
+  const columns: ListGridColumn<ProjectReview>[] = [
+    {
+      header: "Project",
+      key: "project",
+      width: "2fr",
+      render: (item) => (
+        <div className="flex items-center gap-4">
+          <img
+            src={item.avatarUrl}
+            alt={item.name}
+            className="aspect-square size-12 rounded-sm"
+            onError={(event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+              event.currentTarget.src = "/images/grey-image.png";
+            }}
+          />
+          <span>{item.name}</span>
+        </div>
+      ),
+    },
+    {
+      header: "Date Submitted",
+      key: "date",
+      width: "1fr",
+      render: (item) => <IconLabel type="date" date={item.date} />,
+    },
+    {
+      header: "Reviews",
+      key: "reviews",
+      width: "1fr",
+      render: (item) => {
+        const { nApproved, nRejected } = getReviewsCount(item.reviews);
+        return <IconLabel type="reviews" posReviews={nApproved} negReviews={nRejected} />;
+      },
+    },
+    {
+      header: "AI Suggestion",
+      key: "aiSuggestion",
+      width: "1fr",
+      render: (item) => <IconLabel type="ai-evaluation" percent={item.aiSuggestion} />,
+    },
+    {
+      header: "Score Average",
+      key: "scoreAverage",
+      width: "1fr",
+      position: "center",
+      render: (item) => (
+        <div className="flex items-center justify-center">
+          <CircleStat value={item.scoreAverage.toFixed(1)} />
+        </div>
+      ),
+    },
+    {
+      header: "Action",
+      key: "action",
+      width: "2fr",
+      position: "center",
+      render: (item) => {
+        return (
+          <div className="flex items-center justify-center">
+            <ProjectEvaluationAction
+              onEvaluate={action}
+              projectId={item.id}
+              status={evaluationStatus}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <ListGrid
+      data={projects}
+      columns={columns}
+      rowClassName="h-[72px]"
+      getRowKey={(item: ProjectReview) => item.id.toString()}
+    />
+  );
+};
--- src/features/checker/pages/SubmitFinalEvaluationPage/SubmitFinalEvaluationPage.tsx
+++ src/features/checker/pages/SubmitFinalEvaluationPage/SubmitFinalEvaluationPage.tsx
@@ -1,9 +1,98 @@
+import { useState, useMemo } from "react";
+
+import { match } from "ts-pattern";
+
+import { Button } from "@/primitives/Button";
+import { Icon, IconType } from "@/primitives/Icon";
+import { StatCardGroup } from "@/primitives/StatCardGroup";
+
+import { EvaluationAction, ProjectEvaluationList } from "~checker/components";
+import { useGetApplicationsReviewPage } from "~checker/hooks";
+
 export const SubmitFinalEvaluation = () => {
+  const { categorizedReviews, statCardsProps } = useGetApplicationsReviewPage() || {};
+
+  const [projectEvaluations, setProjectEvaluations] = useState<Record<string, boolean>>({});
+
+  const handleUpdateFinalEvaluations = (projectId: string, action: EvaluationAction) => {
+    setProjectEvaluations((prev) => {
+      return match(action)
+        .with("approve", () => ({ ...prev, [projectId]: true }))
+        .with("reject", () => ({ ...prev, [projectId]: false }))
+        .with("skip", () => {
+          if (prev[projectId] !== undefined) {
+            const { [projectId]: _, ...rest } = prev;
+            return rest;
+          }
+          return prev;
+        })
+        .otherwise(() => {
+          return prev;
+        });
+    });
+  };
+
+  const numberOfOnchainEvaluations = useMemo(
+    () => Object.keys(projectEvaluations).length,
+    [projectEvaluations],
+  );
+
+  const handleRecordEvaluationsOnchain = () => {
+    console.log("Record evaluations onchain", projectEvaluations);
+  };
+
+  const ReadyApplicationsToSubmit = categorizedReviews?.READY_TO_REVIEW || [];
+
   return (
-    <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-[12px]">
-        <div className="font-mono text-2xl font-medium leading-loose text-black">
-          Final Evaluation Page
+    <div className="flex flex-col gap-6 px-20 pt-6">
+      <div className="flex flex-col gap-8">
+        <Button
+          value="Exit"
+          icon={<Icon type={IconType.X} className="fill-red-700" />}
+          className="flex h-8 w-fit justify-start gap-2 bg-red-50 p-4 text-red-700"
+        />
+        <StatCardGroup stats={statCardsProps || []} justify="center" />
+      </div>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <div className="font-mono text-2xl font-medium leading-loose text-black">
+            Review applications
+          </div>
+          <div className="font-mono text-base font-normal leading-7 text-grey-900">
+            Evaluate projects here.
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="pb-1">
+            <div className="flex items-center justify-between pb-1">
+              <div className="font-mono text-2xl font-medium leading-loose text-black">
+                {`Ready to submit (${ReadyApplicationsToSubmit.length})`}
+              </div>
+              <div className="flex gap-2">
+                <Button value="cancel" />
+                <Button
+                  value={`Record (${numberOfOnchainEvaluations}) evaluations onchain`}
+                  disabled={Object.keys(projectEvaluations).length === 0}
+                  onClick={handleRecordEvaluationsOnchain}
+                />
+              </div>
+            </div>
+            <div className="h-px bg-grey-300" />
+          </div>
+
+          <div>
+            {ReadyApplicationsToSubmit.length === 0 ? (
+              <div className="font-mono text-base font-normal leading-7 text-grey-900">
+                Evaluations that are ready to be submitted onchain will appear here once reviewed.
+                Manager supports multiple reviewers.
+              </div>
+            ) : (
+              <ProjectEvaluationList
+                projects={ReadyApplicationsToSubmit}
+                action={handleUpdateFinalEvaluations}
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
