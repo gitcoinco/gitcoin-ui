d52e6a2ec0ec0ec7e8ee558e501601d104c81755
Nick Lionis <nikolaos@gitcoin.co>
2024-11-28 17:37:21 +0700

    refactored types (#60)

--- src/features/checker/Checker.tsx
+++ src/features/checker/Checker.tsx
@@ -1,5 +1,26 @@
-import { CheckerRouter, CheckerProps } from "./CheckerRouter";
-import { CheckerProvider } from "./store";
+import { Hex } from "viem";
+
+import { Step } from "@/components/ProgressModal";
+
+import { CheckerRouter } from "~checker/CheckerRouter";
+import { ReviewBody } from "~checker/pages";
+import { EvaluationBody } from "~checker/services/checker/api";
+import { CheckerProvider } from "~checker/store";
+
+export interface CheckerProps {
+  address: Hex;
+  poolId: string;
+  chainId: number;
+  setEvaluationBody: (body: EvaluationBody) => void;
+  isSigning: boolean;
+  isSuccess: boolean;
+  isEvaluating: boolean;
+  isError: boolean;
+  isErrorSigning: boolean;
+  steps: Step[];
+  setReviewBody: (reviewBody: ReviewBody | null) => void;
+  isReviewing: boolean;
+}
 
 export const Checker = (props: CheckerProps) => {
   return (
