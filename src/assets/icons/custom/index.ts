89852ad92bc96908b03d2cd2cdada1829db472b3
Hussein Martinez <husse.dev@gmail.com>
2024-11-28 13:04:40 +0700

    chore: reorganized icons

---
+++ src/assets/icons/custom/index.ts
@@ -0,0 +1,27 @@
+import CheckerIcon from "./checker.svg?react";
+import ExplorerIcon from "./explorer.svg?react";
+import ShineIcon from "./shine.svg?react";
+import UserIcon from "./user.svg?react";
+import VerifiedBadgeIcon from "./verifiedBadge.svg?react";
+
+enum CustomIconType {
+  CHECKER = "checker",
+  EXPLORER = "explorer",
+  SHINE = "shine",
+  USER = "user",
+  VERIFIEDBADGE = "verifiedBadge",
+}
+
+const customIconComponents: Record<CustomIconType, React.FC<React.SVGProps<SVGSVGElement>>> = {
+  checker: CheckerIcon,
+  explorer: ExplorerIcon,
+  shine: ShineIcon,
+  user: UserIcon,
+  verifiedBadge: VerifiedBadgeIcon,
+};
+const customIcons = Object.keys(customIconComponents).sort((a, b) =>
+  a.localeCompare(b),
+) as CustomIconType[];
+
+export { CheckerIcon, ExplorerIcon, ShineIcon, UserIcon, VerifiedBadgeIcon };
+export { CustomIconType, customIconComponents, customIcons };
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,40 +1,10 @@
-// Status Icons
-export { default as CheckIcon } from "./check.svg?react";
-// Solid Check Icon
-export { default as CheckSolidIcon } from "./solid/check.svg?react";
-export { default as ExclamationCircleIcon } from "./exclamationCircle.svg?react";
-export { default as XIcon } from "./x.svg?react";
-export { default as LinkIcon } from "./link.svg?react";
-export { default as clipboardListIcon } from "./clipboardList.svg?react";
-export { default as StarIcon } from "./star.svg?react";
-export { default as InformationCircleIcon } from "./information_circle.svg?react";
-
-// Solid X Icon
-export { default as XSolidIcon } from "./solid/x.svg?react";
-export { default as SolidQuestionMarkCircleIcon } from "./solid/questionMarkCircle.svg?react";
-export { default as ChevronLeftIcon } from "./solid/chevron-left.svg?react";
-
-// Time Icons
-export { default as ClockIcon } from "./clock.svg?react";
-export { default as CalendarIcon } from "./calendar.svg?react";
-
-// Miscellaneous Icons
-export { default as SparklesIcon } from "./sparkles.svg?react";
-export { default as ShineIcon } from "./shine.svg?react";
-export { default as UserIcon } from "./user.svg?react";
-export { default as VerifiedBadgeIcon } from "./verifiedBadge.svg?react";
-export { default as GlobeIcon } from "./globe.svg?react";
-export { default as ExplorerIcon } from "./explorer.svg?react";
-export { default as CheckerIcon } from "./checker.svg?react";
+export * from "./heroicons";
 
 // Social Media Icons
-export { default as GithubIcon } from "./github.svg?react";
-export { default as TwitterIcon } from "./twitter.svg?react";
+export * from "./socialMedia";
 
 // Cryptocurrency Icons
-export { default as ETHIcon } from "./networks/eth.svg?react";
-export { default as PolygonIcon } from "./networks/polygon.svg?react";
-export { default as ArbitrumIcon } from "./networks/arbitrum.svg?react";
-export { default as AvaxIcon } from "./networks/avax.svg?react";
-export { default as BaseIcon } from "./networks/base.svg?react";
-export { default as OptimismIcon } from "./networks/optimism.svg?react";
+export * from "./networks";
+
+// Custom Icons
+export * from "./custom";
