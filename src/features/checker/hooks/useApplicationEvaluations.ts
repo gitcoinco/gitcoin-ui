a108aa2e4e8fe215e5967af523d3121cbe4d5add
Nick Lionis <nikolaos@gitcoin.co>
2024-12-10 11:51:57 +0200

    hooks renaming

--- src/features/checker/hooks/useApplicationEvaluations.ts
+++ src/features/checker/hooks/useApplicationEvaluations.ts
@@ -1,24 +1,35 @@
-import { useCheckerContext } from "@/features/checker/store/hooks/useCheckerContext";
+import { useQuery } from "@tanstack/react-query";
 
-import { generatePoolUUID } from "~checker/utils/generatePoolUUID";
+import { getApplicationByIdFromIndexer } from "~checker/services/allo";
+import { getCheckerApplicationEvaluations } from "~checker/services/checker";
 
-export const useApplicationOverviewEvaluations = ({ applicationId }: { applicationId: string }) => {
-  const { poolId, chainId, poolsData } = useCheckerContext();
-
-  const poolUUID = generatePoolUUID(poolId, chainId);
-
-  const poolData = poolUUID ? poolsData[poolUUID] : null;
-
-  if (!poolData) return null;
-
-  const application = poolData.applications[applicationId];
-  const applicationEvaluations = application?.evaluations ?? [];
-  const evaluationQuestions = poolData.evaluationQuestions;
+export const useApplicationEvaluations = (
+  chainId: number,
+  roundId: string,
+  applicationId: string,
+) => {
+  const query = useQuery({
+    enabled: !!applicationId && !!roundId && !!chainId,
+    queryKey: ["viewApplicationEvaluationsPage", chainId, roundId, applicationId],
+    queryFn: async () => {
+      const application = await getApplicationByIdFromIndexer(chainId, roundId, applicationId);
+      const applicationEvaluations = await getCheckerApplicationEvaluations(
+        chainId,
+        roundId,
+        applicationId,
+      );
+      const data = {
+        application,
+        applicationEvaluations: applicationEvaluations,
+      };
+      return data;
+    },
+  });
 
   return {
-    application,
-    applicationEvaluations,
-    evaluationQuestions,
-    poolData,
+    data: query.data,
+    isLoading: query.isLoading,
+    isError: query.isError,
+    error: query.error,
   };
 };
