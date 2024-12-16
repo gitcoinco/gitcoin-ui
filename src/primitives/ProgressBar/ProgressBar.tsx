dda8e20cfa1d09a52943a781c0fdbfc15d1e1e91
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-12-16 15:57:25 +0530

    add progress bar (#91)
    * add progress bar
    
    * fix

---
+++ src/primitives/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,16 @@
+import { Progress, ProgressVariants } from "@/ui-shadcn/progress";
+
+export interface ProgressBarProps {
+  value: number;
+  className?: string;
+  variant?: ProgressVariants;
+}
+
+export const ProgressBar = ({ value, className, variant }: ProgressBarProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <Progress value={value} className={className} variant={variant} />
+      <p className="text-sm">{value}% complete</p>
+    </div>
+  );
+};
--- src/ui-shadcn/progress.tsx
+++ src/ui-shadcn/progress.tsx
@@ -1,27 +1,50 @@
 import * as React from "react";
 
 import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { tv } from "tailwind-variants";
 
-import { cn } from "@/lib/utils";
+export type ProgressVariants = "default" | "green";
+
+const progressVariants = tv({
+  slots: {
+    root: "relative h-4 w-full overflow-hidden rounded-full",
+    indicator: "size-full flex-1 transition-all",
+  },
+  variants: {
+    variant: {
+      default: {
+        root: "bg-grey-100 dark:bg-black",
+        indicator: "bg-black dark:bg-grey-50",
+      },
+      green: {
+        root: "bg-grey-100",
+        indicator: "bg-moss-700",
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-grey-100 dark:bg-black",
-      className,
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-black transition-all dark:bg-grey-50"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    variant?: "default" | "green";
+  }
+>(({ className, value, variant = "default", ...props }, ref) => {
+  const { root, indicator } = progressVariants({ variant });
+
+  return (
+    <ProgressPrimitive.Root ref={ref} className={`${root()} ${className}`} {...props}>
+      <ProgressPrimitive.Indicator
+        className={indicator()}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
+
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
