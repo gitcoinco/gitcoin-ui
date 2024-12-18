da291d6355046d27a0764f677d373b756e0d0046
Hussein Martinez <husse.dev@gmail.com>
2024-12-18 13:11:49 +0100

    chore: upgrade to vite6

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from "@vitejs/plugin-react-swc";
 import path, { resolve } from "path";
 import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
