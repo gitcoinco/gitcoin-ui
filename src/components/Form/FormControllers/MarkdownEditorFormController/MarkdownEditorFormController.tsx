cef5cd413e5b12e39ba0c81fbaf9abdcf067ea91
Nick Lionis <nikolaos@gitcoin.co>
2025-01-05 20:45:48 +0200

    creates the MarkdownEditor form component

---
+++ src/components/Form/FormControllers/MarkdownEditorFormController/MarkdownEditorFormController.tsx
@@ -0,0 +1,23 @@
+// src/components/Form/MarkdownEditorController.tsx
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+
+import { MarkdownEditor } from "@/primitives/MarkdownEditor";
+
+export interface MarkdownEditorFormControllerProps {
+  name: string;
+}
+
+export const MarkdownEditorFormController: React.FC<MarkdownEditorFormControllerProps> = ({
+  name,
+}) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => <MarkdownEditor value={field.value} onChange={field.onChange} />}
+    />
+  );
+};
