329f14a8b68239ba9beeb2f7077aadab3a04ac00
Nick Lionis <nikolaos@gitcoin.co>
2025-01-17 15:58:34 +0200

    chore: added select-switch-multiselect to the build

--- src/client.ts
+++ src/client.ts
@@ -3,3 +3,4 @@ export * from "./primitives/Markdown";
 export * from "./primitives/MarkdownEditor";
 export * from "./components/Form";
 export * from "./components/GenericProgressForm";
+export * from "./components/MultipleSelect";
